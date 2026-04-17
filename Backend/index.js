e1e51650af3e255f0fe25e88b4374ded08de600d
Kusuma <kusumakella23@gmail.com>
2026-04-17 10:28:17 +0530

    your message

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,27 @@
+const express= require("express")
 const mongoose= require("mongoose")
-const userSchema=mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        unique:true,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    mobile:{
-        type:Number,
-        required:true
-    }
-},{timestamps:true})
+const app= express()
+const cors = require("cors")
+require("dotenv").config()
+const authRoutes=require("./routes/authRoutes")
+const productRoutes=require("./routes/productRoutes")
 
-module.exports=mongoose.model("User",userSchema)
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({
+    extended:true
+}))
+mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>{
+        console.log("DB connected")
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+
+app.get("/",(req,res)=>{
+    res.json({message:"server is running"})
+})
+app.use("/api/auth",authRoutes)
+app.use("/api/product",productRoutes)
+app.listen(5000,()=>console.log("server runns on port 5000"))
